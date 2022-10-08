88354cc6d333059ceb0fc5e51bbc8cfbf49e4bfe
Rohit-Baranwal <89464539+Rohit-Baranwal@users.noreply.github.com>
2022-10-08 23:16:45 +0530

    ReactJS Based Shopping Cart

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import {
+  Route,
+  BrowserRouter
+} from "react-router-dom";
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Cart from './Components/Cart';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <div className="App">
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
